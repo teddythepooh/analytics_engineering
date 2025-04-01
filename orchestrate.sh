a463f66b9b0f8d529a775313728ecf74ebdd4511
teddythepooh <chuatedjesusc@gmail.com>
2025-04-01 16:03:08 -0500

    updated readme; bash script; and added max_y + y_ticks args in garfield park visualization similar to citywide visualization script

--- orchestrate.sh
+++ orchestrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-run_dbt=True
-run_geocoder=True
+run_dbt=False
+run_geocoder=False
 
 shapefiles_dir=/projects/cel/2022-028-public-affairs-analysis/data/shapefiles_and_geocodes/chicago/chicago_communities_SHP
 chicago_shp=$shapefiles_dir/geo_export_29d5f5d1-d63c-4386-9d89-76ae473eea7d.shp
@@ -67,13 +67,18 @@ fi
 
 garfield_park_start_years=(2000 2016)
 
+declare -A garfield_park_max_y=([2000]=4500 [2016]=1600)
+declare -A garfield_park_y_ticks=([2000]=500 [2016]=100)
+
 for start_year in ${garfield_park_start_years[@]}; do
     python $garfield_park_script \
-    --start_year=$start_year \
     --config_file=$geo_config \
     --analytical_table=$output_dir/analytical_table.csv \
     --analytical_table_geocoded=$output_dir/analytical_table_geocoded.csv \
     --chi_shp=$chicago_shp \
+    --start_year=$start_year \
+    --max_y=${garfield_park_max_y[$start_year]} \
+    --y_ticks=${garfield_park_y_ticks[$start_year]} \
     --output_dir=$output_dir \
     && echo "Visualized drug arrests in Garfied Park for $start_year onward."
 done
